798b4fe0fb0549c951088efecefe07ce390f18dd
enzoferrazgalvao-dot <enzo.ferraz.galvao@escola.pr.gov.br>
2026-07-03 08:43:30 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -2,71 +2,71 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+   'Matematica',
+    'O que é uma fração?',
+    'È uma forma de representar partes de um todo '
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'Matematica',
+    'Para que serve a porcentagem?',
+    'Serve para representar uma perte de 100 e facilitar comparações e calculos '
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'Portugues',
+    'O que é um substantivo?',
+    'È a palavra que dá nome a pessoas,lugares,objetos,animais ou sentimentos '
 )
 criaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
-)
-
-criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+   'Portugues ',
+    'O que é um verbo?',
+    'È a palavra que indica uma ação, um estado ou um fenomeno da natureza '
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'Ingles',
+    'Qual é a diferença entre Simple past e present perfect?',
+    'O simple past indica uma ação concluida em um momento especifico do passado, enquanto o present perfect'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'Ingles ',
+    'Quando usamos os model verbs can e must?',
+    'Can expressa habilidade ou possibilidade, e must indica obrigação ou necessidade '
+)
+criaCartao(
+   'filosofia ',
+    'O que é ética na filosofia?',
+    'É o estudo dos principios que orientam o comportamento humano e ajudam a distinguir o certo do errado '
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'Filosofia',
+    'O que é iluminismo?',
+    'Foi um movimento filosofico que valorizava a razão, a ciencia e a liberdade como formas de transformar a sociedade '
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'Artes ',
+    'O que são artes visuais?',
+    'São formas de expressão artistica percebidas principalmente pela visão, como pintura,escultura,desenho e fotografia'
+)
+criaCartao(
+   'Artes',
+    'O que é uma obra de arte?',
+    'È uma criação que expressa ideias,emoções,sentimentos ou visão de mundo de um artista '
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
+    'Progamação',
+    'O que é um algoritimo?',
+    'È uma sequencia de passos ou instruções para resolver um problema ou realizar uma tarefa  '
+)
+
+criaCartao(
+    'Progamação',
+    '',
     'Resposta 3'
 )
 criaCartao(
